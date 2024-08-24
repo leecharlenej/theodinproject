3af83d9ebbe53623923cf0bd625f25c8b354273a
leecharlenej <leecharlenej@gmail.com>
2024-08-24 15:25:08 +0800

    Complete base. Need to add delete and read buttons.

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ This repository is for me to hold all my projects from [The Odin Project](https:
 | CSS   | Odin Landing Page | [(here)](https://github.com/leecharlenej/odin-landing-page)| [(live)](https://leecharlenej.github.io/odin-landing-page/first_attempt/) |Web page development using CSS, margins and paddings and flex.
 | JavaScript | Odin Rock, paper, scissors | [(here)](https://github.com/leecharlenej/odin-rock-paper-scissors) | [(live)](https://leecharlenej.github.io/odin-rock-paper-scissors/) | JavaScript programme for rock, paper, scissors |
 | JavaScript | JS Exercises | [(here)](https://github.com/leecharlenej/javascript-exercises) <br> [(here)](https://github.com/leecharlenej/JavaScript30)| | Practices on JavaScript.
-| DOM   | Shopping List | [(here)](Foundations/shopping-list.html) | [(live)](https://leecharlenej.github.io/theodinproject/odin-shopping-list/) | Shopping list created using DOM manipulation. |
+| DOM   | Shopping List | [(here)](https://github.com/leecharlenej/theodinproject/tree/main/odin-shopping-list) | [(live)](https://leecharlenej.github.io/theodinproject/odin-shopping-list/) | Shopping list created using DOM manipulation. |
 | DOM   | Etch-a-Sketch | [(here)](https://github.com/leecharlenej/odin-etch-a-sketch) | [(live)](https://leecharlenej.github.io/odin-etch-a-sketch/) | Etch-a-Sketch using DOM manipulation. |
 | JavaScript | Calculator | [(here)](https://github.com/leecharlenej/odin-calculator) | [(live)](https://leecharlenej.github.io/odin-calculator/) | Calculator using JavaScript, HTML and CSS. |
 ---
@@ -32,3 +32,4 @@ This repository is for me to hold all my projects from [The Odin Project](https:
 |-------|-------|------------|-------------|--------|
 | HTML & CSS | Odin Sign-up Form | [(here)](https://github.com/leecharlenej/odin-sign-up-form) | [(live)](https://leecharlenej.github.io/odin-sign-up-form/) | Sign-up form exploring more advanced CSS options and form creation. |
 | HTML & CSS | Odin Admin Dashboard | [(here)](https://github.com/leecharlenej/odin-admin-dashboard) | [(live)](https://leecharlenej.github.io/odin-admin-dashboard/) | Admin dashboard using grids. |
+| JavaScript | Odin Library | [(here)]() | [(live)]() | |
--- odin-library/javascript.js
+++ odin-library/javascript.js
@@ -1,10 +1,10 @@
-function Book(title, author, pages, read) {
+function Book(title, author, pages, status) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.status = status;
     this .info =  function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read": "not read yet"}`;
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.status ? "read": "not read yet"}`;
     };
 }
 
@@ -37,3 +37,111 @@ function getBookInfo (libraryArr) {
         console.log(libraryArr[i]);
     }
 }
+
+let allBooksHTML = '';
+myLibrary.forEach(function(book) {
+    allBooksHTML += `
+<div class="book">
+    <hr>
+    <div class="book-header">
+        <div class="title">${book.title}</div>
+        <div class="author">by ${book.author}</div>
+        <div class="pages">${book.pages} pages</div>
+        <div class="status">[${book.read ? 'read' : 'not read'}]</div>
+    </div>
+    <hr>
+    <div class="book-footer">
+         <div class="icons">
+            <button id="deleteButton"><img src="assets/img/book-remove.svg">
+            <span class="icon-caption">Delete</span></button>
+        </div>
+
+        <div class="icons">
+            <button id="readButton"><img src="assets/img/book-edit.svg">
+            <span class="icon-caption">Read</span></button>
+        </div>
+    </div>
+</div>`
+})
+
+document.getElementById("allBooks").innerHTML = allBooksHTML;
+
+/* ================================
+   Add book modal
+   ================================ */
+
+let newLibrary = [];
+
+const addBookButton = document.querySelector("#addBookButton");
+const formDialog = document.querySelector("#formDialog");
+const formDialogBody = document.querySelector("#formDialogBody")
+
+addBookButton.addEventListener("click", () => {
+    formDialog.showModal();
+});
+
+const addButton = formDialog.querySelector("#addButton");
+const actionOutcome = document.querySelector(".action-outcome");
+
+formDialog.addEventListener("close", (e) => {
+
+    formDialogBody.reset();
+
+    actionOutcome.innerHTML =
+      formDialog.returnValue === "cancel"
+        ? "No book added!"
+        : `You added:<br>${formDialog.returnValue}.`;
+    console.log(formDialog.returnValue);
+  });
+
+addButton.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const title = formDialog.querySelector("input[name='title']").value;
+    const author = formDialog.querySelector("input[name='author']").value;
+    const pages = formDialog.querySelector("input[name='pages']").value
+    const status = formDialog.querySelector("input[name='status']:checked").value;
+
+    const newBook = new Book (title, author, pages, status);
+
+    let newBookHTML = '';
+    newBookHTML += `
+<div class="book">
+    <hr>
+    <div class="book-header">
+        <div class="title">${newBook.title}</div>
+        <div class="author">by ${newBook.author}</div>
+        <div class="pages">${newBook.pages} pages</div>
+        <div class="status">[${newBook.read ? 'read' : 'not read'}]</div>
+    </div>
+    <hr>
+    <div class="book-footer">
+         <div class="icons">
+            <button id="deleteButton"><img src="assets/img/book-remove.svg">
+            <span class="icon-caption">Delete</span></button>
+        </div>
+
+        <div class="icons">
+            <button id="readButton"><img src="assets/img/book-edit.svg">
+            <span class="icon-caption">Read</span></button>
+        </div>
+    </div>
+</div>`
+
+    document.getElementById("allBooks").innerHTML += newBookHTML;
+    formDialog.close(`${newBook.title}`);
+});
+
+let readButton = document.querySelector("#readButton");
+readButton.addEventListener("click", changeReadStatus);
+
+/* ================================
+   Book icons
+   ================================ */
+
+function changeReadStatus (event) {
+    let newReadStatus = document.querySelector(".book-header > .status");
+    console.log("check:", newReadStatus);
+}
+
+

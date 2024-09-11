2803d3f1087ff74d8cc44485cdb6752b95bcc1a7
leecharlenej <leecharlenej@gmail.com>
2024-09-11 19:19:13 +0800

    Upload files for Asynchronous JavaScript and APIs.

---
+++ Full Stack JavaScript/02. JavaScript/Asynchronous JavaScript and APIs/async-await-test.js
@@ -0,0 +1,23 @@
+async function f() {
+    return 1;
+}
+
+f().then(alert);
+
+
+async function loadJson(url) {
+    const response = await fetch(url)
+    
+
+    return fetch(url)
+      .then(response => {
+        if (response.status == 200) {
+          return response.json();
+        } else {
+          throw new Error(response.status);
+        }
+      });
+  }
+  
+  loadJson('https://javascript.info/no-such-user.json')
+    .catch(alert); // Error: 404
